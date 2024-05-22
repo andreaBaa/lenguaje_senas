02f1879dd74e87ca937a064ae1f3880100d5065e
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-05-22 11:22:41 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,1 +1,54 @@
+import streamlit as st
+from PIL import Image 
 
+st.title(" ¡Aprende Lenguaje de Señas Colombiano!" )
+
+st.write("""
+### Más allá de las palabras, descubre el poder del lenguaje de señas colombiano 
+
+Bienvenido a nuestra plataforma de aprendizaje del Lenguaje de Señas Colombiano (LSC). 
+En Colombia y el mundo, el lenguaje de señas es una herramienta vital para la comunicación inclusiva. 
+Esta plataforma tiene como objetivo facilitar el aprendizaje de LSC, permitiendo a más personas comunicarse efectivamente con la comunidad sorda.
+""")
+
+image = Image.open('LenguajeSeñas1.jpg')
+new_width = 500
+new_height = 300
+image_resized = image.resize((new_width, new_height))
+st.image(image_resized)
+
+st.write("""
+### ¿Por qué es importante aprender LSC?
+
+El Lenguaje de Señas Colombiano es la lengua materna de muchas personas sordas en nuestro país, Colombia cuenta con 439.772 ciudadanos sordos, según la Encuesta Nacional de Calidad de Vida 2022. Aprender LSC se vuelve vital porque:
+
+- Promueve la inclusión y la comunicación efectiva.
+- Fortalece las relaciones personales y profesionales.
+- Ayuda a derribar barreras de comunicación y a crear una sociedad más justa.
+""")
+
+st.write("""
+
+Mira este video que te dará una visión general del Lenguaje de Señas Colombiano y te motivará en tu proceso de aprendizaje.
+""")
+video_url = "https://www.youtube.com/watch?v=tJz9iwuU6aY" 
+st.video(video_url)
+
+st.write("""
+### ¿Qué podrás aprender y encontrar en esta plataforma? 
+
+El propósito  es proporcionar una plataforma accesible y amigable para aprender LSC de manera facil. A través de nuestros módulos, aprenderás:
+
+1. *Básico: El Abecedario*:
+Comenzarás con lo más esencial, el abecedario.
+
+2. *Básico: Tu nombre*:
+Aprenderás a deletrear tu nombre y presentarte.
+
+3. *Básico: Tu propia seña*:
+Crearás tu propia seña, será una forma única de ser reconocida y comunicarte en la comunidad sorda.
+""")
+
+
+st.write("""
+## ¡Comencemos!""")
