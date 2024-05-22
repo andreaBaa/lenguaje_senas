76c90b411c3568be3bffd376e3bf7ef241ee252b
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-05-22 16:55:44 -0500

    Update Básico: abecedario.py

--- pages/Básico: abecedario.py
+++ pages/Básico: abecedario.py
@@ -1,2 +1,5 @@
+import streamlit as st
+
+
 if st.button('Ir a módulo Básico: abecedario'):
     st.markdown('<meta http-equiv="refresh" content="0; url=https://abcbasico.streamlit.app/">', unsafe_allow_html=True)
