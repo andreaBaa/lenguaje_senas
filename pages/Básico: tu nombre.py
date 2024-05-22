eae7823b12bcf580ab03e2c808667bc7896730ec
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-05-22 13:04:02 -0500

    Update Básico: tu nombre.py

--- pages/Básico: tu nombre.py
+++ pages/Básico: tu nombre.py
@@ -211,6 +211,6 @@ if nombre:
 
         st.write("Ya puedes dirigirte al siguiente módulo **'Básico: Tu Propia Seña'**")
 
-        st.markdown("[Siguiente módulo: Básico: Tu Propia Seña](https://aprendelenguajedesenas.streamlit.app/B%C3%A1sico:_tu_propia_se%C3%B1a)")
+        st.markdown("[Siguiente módulo: Básico: Tu Propia Seña](https://aprendelenguajedesenas.streamlit.app/B%C3%A1sico:_tu_propia_se%C3%B1a)", unsafe_allow_html=True)
         
 
