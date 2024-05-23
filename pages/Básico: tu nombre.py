42e27e091b102adfbf78af420ae7284d787fdeb5
andreaBaa <160878920+andreaBaa@users.noreply.github.com>
2024-05-22 21:05:12 -0500

    Update Básico: tu nombre.py

--- pages/Básico: tu nombre.py
+++ pages/Básico: tu nombre.py
@@ -1,10 +1,33 @@
 import streamlit as st 
-
 import os 
-
 import random 
+import paho.mqtt.client as paho
+import time
+import json
+import cv2
+import numpy as np
 
-  
+def on_publish(client,userdata,result):             #create function for callback
+    print("el dato ha sido publicado \n")
+    pass
+
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received=str(message.payload.decode("utf-8"))
+    st.write(message_received)
+
+        
+
+broker="broker.mqttdashboard.com"
+port=1883
+client1= paho.Client("APP_CERR")
+client1.on_message = on_message
+client1.on_publish = on_publish
+client1.connect(broker,port)
+
+
+
 
 # Título y Subtítulo 
 
